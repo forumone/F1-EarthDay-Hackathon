5d1d70954cc4f9f938468dd3daf9cf50d969b641
William Hurley <whurley@forumone.com>
2016-04-22 16:21:19 -0400

    Removing layer

--- src/js/states/home/index.js
+++ src/js/states/home/index.js
@@ -4,30 +4,6 @@ angular.module('wildfireHazards').controller('HomeController', function($scope, 
     var latlng = place.geometry.location;
     map.jumpTo({ center : [latlng.lng(), latlng.lat()], zoom : 14 });
     
-    map.addSource('symbols', {
-      type : 'geojson',
-      data : {
-        type : 'FeatureCollection',
-        features : [{
-          type : 'Feature',
-          properties : {},
-          geometry : {
-            type : 'Point',
-            coordinates : [latlng.lng(), latlng.lat()]
-          }
-        }]
-      }
-    });
-    
-    map.addLayer({
-      "id": "symbols",
-      "type": "symbol",
-      "source": "symbols",
-      "layout": {
-          "icon-image": "marker-15"
-      },
-      "paint": {}
-    });
     
     map.on('click', function (e) {
       mapLocation.setLat(latlng.lat());
